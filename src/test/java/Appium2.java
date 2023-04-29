2be1dcfd7ed80e8cd82e64a911e56037b523f573
ahmed zakria <48220388+ahmedzak2@users.noreply.github.com>
2023-04-29 20:34:16 +0300

    one

---
+++ src/test/java/Appium2.java
@@ -0,0 +1,31 @@
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Appium2 {
+   // AppiumDriver driver2;
+   private AndroidDriver driver;
+    String url = "http://localhost:4723/wd/hub";
+    @Test
+    public void appiumTest() throws MalformedURLException, InterruptedException {
+
+        /*to set and show the specification of app we use class UiAutomator2Options
+        * */
+        DesiredCapabilities options=new DesiredCapabilities();
+       // options.setDeviceName("sdk_gphone64_x86_64");
+        options.setCapability("deviceName","sdk_gphone64_x86_64");
+        options.setCapability("platformName","Android");
+      options.setCapability("platformVersion","12");
+       // options.setAutomationName("Appium");
+        options.setCapability("app","C:\\Users\\20112\\IdeaProjects\\appium\\src\\test\\resources\\ApiDemos-debug.apk");
+     //options.setApp(System.getProperty("C:\\Users\\20112\\IdeaProjects\\appium\\src\\test\\resources\\ApiDemos-debug.apk"));
+      //  driver2 = new AppiumDriver(new URL(url),options);
+        driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), options);
+    Thread.sleep(10000);
+        driver.quit();
+    }
+}
