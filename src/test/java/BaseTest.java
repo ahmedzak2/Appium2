6cb0cc52f36db566db6e2385605b1a5c70a490cf
ahmed zakria <48220388+ahmedzak2@users.noreply.github.com>
2023-05-02 20:59:26 +0300

    Add files via upload

---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,39 @@
+package src.test.java;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.time.Duration;
+
+public class BaseTest {
+    public AndroidDriver driver;
+public    String url = "http://localhost:4723/wd/hub";
+@BeforeTest
+    public void setup() throws MalformedURLException {
+        UiAutomator2Options options=new UiAutomator2Options();
+        // options.setDeviceName("sdk_gphone64_x86_64");
+        options.setDeviceName("sdk_gphone64_x86_64");
+        //options.setPlatformName("Android");
+        options.setPlatformVersion("11");
+        // options.setAutomationName("Appium");
+        options.setApp("D:\\learning\\Appium2-main\\src\\test\\java\\src\\test\\resources\\ApiDemos-debug.apk");
+        //options.setApp(System.getProperty("C:\\Users\\20112\\IdeaProjects\\appium\\src\\test\\resources\\ApiDemos-debug.apk"));
+        //  driver2 = new AppiumDriver(new URL(url),options);
+        driver = new AndroidDriver(new URL(url),options);
+        /*different locator appium surrport
+         * ID -Name -Class Name - Accessibility ID - Xpath - ccs clestor -UIAUtomator */
+
+    /*
+    * to specify the maxium time for appium to search or wait for element to show  in display
+    * */
+   driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+    @AfterTest
+    public void tearDown(){
+    driver.quit();
+    }
+}
